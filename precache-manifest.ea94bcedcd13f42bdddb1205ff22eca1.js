0349e5b01b6ba1466381c0b5e4f92d9fe0bd6053
Alan Guo <aguo@semanticmachines.com>
2020-03-09 21:29:26 -0400

    Updates

---
+++ precache-manifest.ea94bcedcd13f42bdddb1205ff22eca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea1a7486be6e6606d57a127522395e6",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a531d5f41deba43cd311",
+    "url": "/tic-tac-toe/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8c14a14a42f34cfe2ebc",
+    "url": "/tic-tac-toe/static/css/main.8f163830.chunk.css"
+  },
+  {
+    "revision": "a531d5f41deba43cd311",
+    "url": "/tic-tac-toe/static/js/2.2880c001.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/tic-tac-toe/static/js/2.2880c001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c14a14a42f34cfe2ebc",
+    "url": "/tic-tac-toe/static/js/main.b7552678.chunk.js"
+  },
+  {
+    "revision": "fdbd6fae367d142c593b",
+    "url": "/tic-tac-toe/static/js/runtime-main.df4931a8.js"
+  }
+]);
